d54fec24cba5852dc063f2c8c045cb97b0aa9373
Clayton Watkins <watkinclayton17@yahoo.com>
2020-04-07 19:34:14 -0700

    Set up color model

---
+++ Rainbow/Model/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow
+//
+//  Created by Clayton Watkins on 4/7/20.
+//  Copyright © 2020 Clayton Watkins. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    var colorName: String
+    var color: UIColor
+}
